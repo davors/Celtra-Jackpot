eec80c929eb67c9ede5dd034e018d00f133a4523
davors <davor.sluga@gmail.com>
2014-10-06 23:28:00 +0200

    added changePointDetection.py

--- Jackpot/Jackpot.py
+++ Jackpot/Jackpot.py
@@ -1,4 +1,5 @@
 import configuration
+from changePointDetection import checkChange
 from fileIO import *
 
 #for c in range(1,configuration.N_CASES+1):
@@ -14,4 +15,9 @@ from fileIO import *
 #        writeDataToFile(c,machines,pulls,reps)
 
 (case, machines, pulls, reps, data)=loadDataFromFile('case_01_02m_00500p.txt')
-print str(case)    
+Y=[]
+X=0
+m=2
+for i in range(1,10):
+    X=X+data[m-1][i-1]
+    (s,t,Y)=checkChange(X, Y, i, data[m-1][0:i])
