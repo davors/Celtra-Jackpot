eceba9bcf4ba0b70b33a149f145a244307c5a325
davors <davor.sluga@gmail.com>
2014-10-06 14:30:46 +0200

    Fixed loadDataFromFile

--- Jackpot/Jackpot.py
+++ Jackpot/Jackpot.py
@@ -1,15 +1,17 @@
 import configuration
 from fileIO import *
 
-for c in range(1,configuration.N_CASES+1):
-    machines=getNumMachines(c)
-    pulls=getNumPulls(c)
-    reps=1000
+#for c in range(1,configuration.N_CASES+1):
+#    machines=getNumMachines(c)
+#    pulls=getNumPulls(c)
+#    reps=1
     
-    #if c<=5: 
-    #    reps=20000/pulls
-    #else: 
-    #    reps=20000
-    if configuration.TEST==1:
-        writeDataToFile(c,machines,pulls,reps)
-    
+#    #if c<=5: 
+#    #    reps=20000/pulls
+#    #else: 
+#    #    reps=20000
+#    if configuration.TEST==1:
+#        writeDataToFile(c,machines,pulls,reps)
+
+(case, machines, pulls, reps, data)=loadDataFromFile('case_01_02m_00500p.txt')
+print str(case)    
