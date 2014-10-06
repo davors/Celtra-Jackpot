851e3cbe48a65425ff07eec50bd14828305c31ff
hruska1 <vodopivec.tom@gmail.com>
2014-10-06 14:35:04 +0200

    Commit Tom again

--- Jackpot/Jackpot.py
+++ Jackpot/Jackpot.py
@@ -1,15 +1,21 @@
 import configuration
 from fileIO import *
+from SampleAnalyzer import *
 
-for c in range(3,configuration.N_CASES+1):
-    machines=getNumMachines(c)
-    pulls=getNumPulls(c)
-    reps=1000
+
+SampleAnalyzer('case_06_02m_01000p_1000r.txt')
+
+
+#for c in range(3,configuration.N_CASES+1):
+
+    #machines=getNumMachines(c)
+    #pulls=getNumPulls(c)
+    #reps=1000
     
-    #if c<=5: 
-    #    reps=20000/pulls
-    #else: 
-    #    reps=20000
-    if configuration.TEST==1:
-        writeDataToFile(c,machines,pulls,reps)
+    ##if c<=5: 
+    ##    reps=20000/pulls
+    ##else: 
+    ##    reps=20000
+    #if configuration.TEST==1:
+    #    writeDataToFile(c,machines,pulls,reps)
     
