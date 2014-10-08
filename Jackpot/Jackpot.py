3859854bc2a5f0459527aa992c9daad4b4ef506a
davors <davor.sluga@gmail.com>
2014-10-08 16:19:20 +0200

    Added strategy.py

--- Jackpot/Jackpot.py
+++ Jackpot/Jackpot.py
@@ -2,6 +2,8 @@ import configuration
 from changePointDetection import *
 from fileIO import *
 from SampleAnalyzer import *
+from machine import *
+from strategy import *
 #for c in range(1,configuration.N_CASES+1):
 #    machines=getNumMachines(c)
 #    pulls=getNumPulls(c)
@@ -13,20 +15,45 @@ from SampleAnalyzer import *
 #    if configuration.TEST==1:
 #        writeDataToFile(c,machines,pulls,reps)
 (case, machines, pulls, reps, data)=loadDataFromFile('case_06_02m_01000p.txt')
-Y=[]
-X=0
-m=2
-Y2=0
-HP=0
-mt=0
-Mt=0
+M=[machine(m+1,pulls) for m in range(machines)]
+
+for i in range(0,500):
+    data[0][i]=random.random()<0.9
+    data[1][i]=random.random()<0.1
+
+for i in range(500,1000):
+    data[0][i]=random.random()<0.1
+    data[1][i]=random.random()<0.9
+
 for i in range(1,pulls+1):
-    X=X+data[m-2][i-1]
-    (s,A,Y)=checkChange(2.5,X, Y, i, data[m-2][0:i])
-    #(s,A,Y)=checkChange(1.96,X, Y, i, data[m-2][0:i])
-    if(s>0):
-        print 'checkChange triggered at: '+str((s,A,Y,i))
-        break
+    sM=EGreedy(M,0.1)
+    r=data[sM.id-1][sM.totalN]
+    sM.pull(r)
+    if i%100==0:
+        for m in M:
+            m.resetState()
+
+totalReward=0
+for m in M:
+    totalReward=totalReward+m.totalReward
+    print 'Machine '+str(m.id)+' Total reward: '+str(m.totalReward)+' Total pulls: '+str(m.totalN)+' Average reward: '+str(m.mR)
+
+print 'Total reward: '+str(totalReward)
+
+#Y=[]
+#X=0
+#m=2
+#Y2=0
+#HP=0
+#mt=0
+#Mt=0
+#for i in range(1,pulls+1):
+#    X=X+data[m-2][i-1]
+#    (s,A,Y)=checkChange(2.5,X, Y, i, data[m-2][0:i])
+#    #(s,A,Y)=checkChange(1.96,X, Y, i, data[m-2][0:i])
+#    if(s>0):
+#        print 'checkChange triggered at: '+str((s,A,Y,i))
+#        break
     #(status, HP, Mt, Y2)=HankeyPankeyTest(80,HP, Mt,data[m-2][i-1], Y2, i)
     #if(status==1):
     #    print 'HankeyPankey triggered at: '+str((HP,i))
